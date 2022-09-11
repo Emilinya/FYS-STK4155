57a8e5d42ff021d1d86739dbf098696101a602e7
Emilie <emilid2103@gmail.com>
2022-09-11 18:21:53 +0200

    back to frankeFunction

--- project1/MSE_R2_plotter.py
+++ project1/MSE_R2_plotter.py
@@ -1,13 +1,10 @@
 from linfitUtils import get_design_matrix, get_beta, test_fit
+from franke_func import frankeFunction
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
-def simplefunc(x):
-    term1 = 0.75*np.exp(-0.25*(9*x-2)**2)
-    term2 = 0.75*np.exp(-((9*x+1)**2)/49.0)
-    return term1 + term2
-
-def plot_MSE_R2_fig(x_ray, y_ray, max_degree, filename, scaling=False):
+def plot_MSE_R2_fig(x_grid, y_grid, z_grid, max_degree, filename, scaling=False):
     degree_list = list(range(max_degree+1))
     MSE_train_list = []
     R2_train_list = []
@@ -16,15 +13,19 @@ def plot_MSE_R2_fig(x_ray, y_ray, max_degree, filename, scaling=False):
 
     if scaling:
         # first subtract mean
-        x_ray = x_ray - np.average(x_ray)
-        y_ray = y_ray - np.average(y_ray)
+        x_grid = x_grid - np.average(x_grid)
+        y_grid = y_grid - np.average(y_grid)
+        z_grid = z_grid - np.average(z_grid)
 
-        # then scale such that max = 1
-        x_ray = x_ray / np.max(x_ray)
-        y_ray = y_ray / np.max(y_ray)
+        # then scale such that largest absolute value is 1
+        x_grid = x_grid / np.max(np.abs(x_grid))
+        y_grid = y_grid / np.max(np.abs(y_grid))
+        z_grid = z_grid / np.max(np.abs(z_grid))
 
-    for poly_degree in degree_list:
-        X_train, X_test, y_train, y_test = get_design_matrix(poly_degree, x_ray, y_ray)
+    start = time.time()
+    for i, poly_degree in enumerate(degree_list):
+        print(f"\r  {i}/{max_degree}", end="")
+        X_train, X_test, y_train, y_test = get_design_matrix(poly_degree, x_grid, y_grid, z_grid)
         beta = get_beta(X_train, y_train)
         
         MSE_train, R2_train = test_fit(y_train, X_train, beta)
@@ -34,6 +35,7 @@ def plot_MSE_R2_fig(x_ray, y_ray, max_degree, filename, scaling=False):
         MSE_test, R2_test = test_fit(y_test, X_test, beta)
         MSE_test_list.append(MSE_test)
         R2_test_list.append(R2_test)
+    print(f" - {time.time() - start:.3f} s")
 
     plt.plot(degree_list, MSE_train_list, label="training MSE")
     plt.plot(degree_list, R2_train_list, label="training R2")
@@ -41,18 +43,23 @@ def plot_MSE_R2_fig(x_ray, y_ray, max_degree, filename, scaling=False):
     plt.plot(degree_list, R2_test_list, label="testing R2")
     plt.xlabel("polynomial degree []")
     plt.ylabel("y []")
+    plt.title(f"MSE and R2 of polynomial fits with different degree, {'scaled data' if scaling else 'no scaling'}, n={n}")
     plt.legend()
     plt.savefig(filename, dpi=200)
     plt.clf()
 
     return degree_list, R2_test_list
 
-n = 100000
-x_ray = np.linspace(0, 1, n)
-y_ray = simplefunc(x_ray)
+for n in [5, 20, 100, 400]:
+    x_ray = np.linspace(0, 1, n)
+    x_grid, y_grid = np.meshgrid(x_ray, x_ray)
+    z_grid = frankeFunction(x_grid, y_grid)
 
-degree_list, R2_test_noscale = plot_MSE_R2_fig(x_ray, y_ray, 6, "imgs/MSE_R2_noscale.png")
-_, R2_test_scale = plot_MSE_R2_fig(x_ray, y_ray, 6, "imgs/MSE_R2_scale.png", scaling=True)
-plt.plot(degree_list, np.array(R2_test_noscale) - np.array(R2_test_scale), label="R2 no scale - R2 scale")
-plt.legend()
-plt.savefig("imgs/test.png", dpi=200)
+    print(f"n = {n}:")
+    degree_list, R2_test_noscale = plot_MSE_R2_fig(x_grid, y_grid, z_grid, 6, f"imgs/MSE_R2_comp/MSE_R2_n={n}_noscale.png")
+    _, R2_test_scale = plot_MSE_R2_fig(x_grid, y_grid, z_grid, 6, f"imgs/MSE_R2_comp/MSE_R2_n={n}_scale.png", scaling=True)
+
+    plt.plot(degree_list, np.array(R2_test_noscale) - np.array(R2_test_scale), label="R2 no scale - R2 scale")
+    plt.legend()
+    plt.savefig(f"imgs/MSE_R2_comp/MSE_R2_n={n}_diff.png", dpi=200)
+    plt.clf()
--- project1/linfitUtils.py
+++ project1/linfitUtils.py
@@ -1,24 +1,25 @@
 import numpy as np
+from polyUtils import power_itr
 from sklearn.model_selection import train_test_split
 
-def get_design_matrix(poly_degree, x_ray, y_ray):
+def get_design_matrix(poly_degree, x_grid, y_grid, z_grid):
     X = np.array(
         [[
-            x**d for d in range(poly_degree+1)
-        ] for x in x_ray]
+            x**xd * y**yd for xd, yd in power_itr(poly_degree)
+        ] for x, y in zip(x_grid.flat, y_grid.flat)]
     )
-    X_train, X_test, y_train, y_test = train_test_split(X, y_ray, test_size=0.2)
+    X_train, X_test, y_train, y_test = train_test_split(X, z_grid.flat, test_size=0.2)
 
     return X_train, X_test, y_train, y_test
 
 def get_beta(X, y):
-    return np.linalg.pinv(X.T @ X) @ X.T @ y
+    return np.linalg.solve(X.T @ X, X.T @ y)
 
 def get_fit(X, beta):
     return X @ beta
 
 def MSE_R2(y_dat, y_fit):
-    n = len(y_dat.flat)
+    n = len(y_dat)
     average = np.sum(y_dat) / n
     squared_error = np.sum((y_dat - y_fit)**2)
     return squared_error / n, 1 - squared_error / np.sum((y_dat - average)**2)
